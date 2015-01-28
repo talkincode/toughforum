e0d28802c3d77768812473588afce966d93f900e
jamiesun <jamiesun.net@gmail.com>
2015-01-28 15:17:38 +0800

    add md support

--- handlers/base.py
+++ handlers/base.py
@@ -36,7 +36,7 @@ cache = CacheManager(cache_regions={
 
 def markdown(src):
     ext = ['code-friendly','cuddled-lists','smarty-pants','spoiler'
-    'fenced-code-blocks','footnotes','tables','tag-friendly']
+    'fenced-code-blocks','metadata','tables','tag-friendly']
     return  markdown2.markdown(src,extras=ext)
 
 ##############################################################################
